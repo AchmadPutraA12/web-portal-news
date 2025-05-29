7d1058d24e4d080d6bc70844b5e6b28eb1115e66
AchmadPutraA12 <ahmad.arifki11@gmail.com>
2025-05-29 21:25:45 +0700

    fix: layout article

--- resources/views/pages/guest/article-detail.blade.php
+++ resources/views/pages/guest/article-detail.blade.php
@@ -2,10 +2,7 @@
 
 @section('content')
     <div class="container py-5">
-        {{-- Judul Artikel --}}
         <h1 class="fw-bold mb-3">{{ $article->title }}</h1>
-
-        {{-- Meta info: tanggal dan kategori --}}
         <p class="text-muted mb-4">
             {{ $article->created_at->format('d M Y') }} |
             Kategori:
@@ -14,18 +11,15 @@
             </a>
         </p>
 
-        {{-- Gambar utama --}}
         @if ($article->image_1)
             <img src="{{ asset('storage/' . $article->image_1) }}" alt="{{ $article->title }}" class="img-fluid mb-4 rounded"
                 style="max-height: 500px; object-fit: cover; width: 100%;">
         @endif
 
-        {{-- Isi artikel --}}
         <div class="article-content mb-4">
             {!! $article->content_article !!}
         </div>
 
-        {{-- Jika layout bukan 1, tampilkan image_2 dan image_3 setelah isi --}}
         @if ($article->layout != 1)
             <div class="row mb-4">
                 @if ($article->image_2)
